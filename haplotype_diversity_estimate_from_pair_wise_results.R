ce2146c33c39b4f527034c5b82e745e17130b38c
PingFan6 <37794678+PingFan6@users.noreply.github.com>
2019-11-28 12:51:01 +0800

    Update haplotype_diversity_estimate_from_pair_wise_results.R

--- haplotype_diversity_estimate_from_pair_wise_results.R
+++ haplotype_diversity_estimate_from_pair_wise_results.R
@@ -1,6 +1,6 @@
 ###############################################
 # A new approach for estimating haplotype diversity from mitochondrial DNA 
-#Ping Fan, Jon Fjeldså, Xuan Liu, Yafei Dong, Yongbin Chang, Yanhua Qu, Gang Song,Fumin Lei
+# Ping Fan, Jon Fjeldså, Xuan Liu, Yafei Dong, Yongbin Chang, Yanhua Qu, Gang Song,Fumin Lei
 ## Code in this file by Ping Fan
 ###############################################
 
