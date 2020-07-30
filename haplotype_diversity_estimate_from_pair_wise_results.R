ba464d845d73856020ab6da723c8bd67079035d0
PingFan6 <37794678+PingFan6@users.noreply.github.com>
2020-07-30 09:47:28 +0800

    Update haplotype_diversity_estimate_from_pair_wise_results.R

--- haplotype_diversity_estimate_from_pair_wise_results.R
+++ haplotype_diversity_estimate_from_pair_wise_results.R
@@ -7,7 +7,7 @@
 # PW_result is the result output by run_pairwise_function/random_pairwise_function, whose file name contain the "result".
 # GD_function is use to calaulate the haplotype diversity and nucleotide diversity from pairwise results. 
 library(dplyr)
-GD_function(PW_result) <- function{
+GD_function <- function(PW_result){
 pair_wise_result <- read.data(PW_result,header = TRUE)
 sel_result <- filter(pair_wise_result,num_per_dp > 0)
 hd <- length(sel_result$num_per_dp)/length(pair_wise_result$num_per_dp)
